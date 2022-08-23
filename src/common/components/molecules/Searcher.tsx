fe49002eb5e679a44a4748e6763d759e153929eb
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2022-08-23 15:39:45 -0600

    add Searcher component

---
+++ src/common/components/molecules/Searcher.tsx
@@ -0,0 +1,16 @@
+import { Search } from 'react-feather'
+
+export const Searcher = () => {
+  return (
+    <div>
+      <div>
+        <Search />
+      </div>
+      <div>
+        <form action="">
+          <input type="text" />
+        </form>
+      </div>
+    </div>
+  )
+}
