b819fdb583d342499363f47df8a9cb3728b7738f
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2022-08-23 19:18:42 -0600

    update api functions

--- src/common/api/api.api.ts
+++ src/common/api/api.api.ts
@@ -1,7 +1,8 @@
+import { ICountry } from '@interfaces/api/country.interface'
 import axios from 'axios'
 
 const axiosInstance = axios.create({
   baseURL: 'https://restcountries.com/v3.1/'
 })
 
-export const getApi = (url: string) => axiosInstance.get(url)
+export const getApi = <T = any>(url: string) => axiosInstance.get<T>(url)
--- src/common/api/contry.api.ts
+++ src/common/api/contry.api.ts
@@ -1,4 +1,24 @@
+import { ICountry } from '@interfaces/api/country.interface'
 import { getApi } from './api.api'
 
-export const getAllCountries = () => getApi('all')
-export const getCountryByName = (name: string) => getApi(`name/${name}`)
+export const getAllCountries = async (): Promise<ICountry[] | never[]> => {
+  try {
+    const response = await getApi<ICountry[]>('all')
+
+    return response.data
+  } catch (error) {
+    console.log(error)
+    return []
+  }
+}
+
+export const getCountryByName = async (name: string): Promise<ICountry | null> => {
+  try {
+    const response = await getApi<ICountry>(`name/${name}`)
+
+    return response.data
+  } catch (error) {
+    console.log(error)
+    return null
+  }
+}
